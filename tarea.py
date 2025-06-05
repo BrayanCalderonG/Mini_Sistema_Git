f28a3b8f2c812a86750ce1fbbaa9a9bf58bf882a
BrayanCalderonG <141978914+BrayanCalderonG@users.noreply.github.com>
2025-06-05 13:23:00 -0600

    tarea.py

--- tarea.py
+++ tarea.py
@@ -1,24 +1,15 @@
-# tarea.py
-
 class Tarea:
     def __init__(self, nombre, descripcion):
-        """
-        Constructor de la clase Tarea.
-        Inicializa una tarea con nombre, descripción y estado de completada en False.
-        """
         self.nombre = nombre
         self.descripcion = descripcion
         self.completada = False
 
     def marcar_completada(self):
-        """
-        Marca la tarea como completada.
-        """
         self.completada = True
 
+    def desmarcar_completada(self):
+        self.completada = False
+
     def __str__(self):
-        """
-        Representación en texto de la tarea.
-        """
-        estado = "Completada" if self.completada else "Pendiente"
-        return f"Tarea: {self.nombre}\nDescripción: {self.descripcion}\nEstado: {estado}"
+        estado = "✅ Completada" if self.completada else "❌ Pendiente"
+        return f"Tarea: {self.nombre} - {self.descripcion} [{estado}]"
